c6ba680f0ed2526bcfa5723897903e41420547db
Kewei Zhang <jeffzhkw@gmail.com>
2021-11-02 08:25:44 -0400

    add comment

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const express = require("express");
+// Initiate Express
 const app = express();
-
+//Setting port - dynamically with heroku
 port = process.env.PORT || 4000;
 
+//Routes for directing user to correct place
 const indexRouter = require("./routes/index");
 const articleRouter = require("./routes/article");
 const createArticleRoute = require("./routes/createArticle");
 
+//Tell express to use routes
 app.use("/", indexRouter);
 app.use("/article", articleRouter);
 app.use("/create", createArticleRoute);
 
+//Listen for requrest on the port - if a user visits then send a result.
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}.`);
 });
