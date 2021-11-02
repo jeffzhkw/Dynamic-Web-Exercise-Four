395be2146c9c1e7873363d545159c104d5398486
Kewei Zhang <jeffzhkw@gmail.com>
2021-11-02 08:59:48 -0400

    start firebase, need fix

--- index.js
+++ index.js
@@ -4,6 +4,21 @@ const app = express();
 //Setting port - dynamically with heroku
 port = process.env.PORT || 4000;
 
+//Import firebase
+const firebase = require("firebase/app");
+//Get configuration object so we can communicate with Firebase
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD73OxLyJqvEkV1wTNtcCLCcyXuAYxk3Io",
+  authDomain: "dynamic-web-exercise-fou-b6e9c.firebaseapp.com",
+  projectId: "dynamic-web-exercise-fou-b6e9c",
+  storageBucket: "dynamic-web-exercise-fou-b6e9c.appspot.com",
+  messagingSenderId: "723704793571",
+  appId: "1:723704793571:web:1f17b99242c054386db8d6",
+};
+//Init firebase
+firebase.initializeApp(firebaseConfig); //const app = initializeApp(firebaseConfig);
+
 //Routes for directing user to correct place
 const indexRouter = require("./routes/index");
 const articleRouter = require("./routes/article");
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const firebase = require("firebase/app");
 
+//Init firebase database
+const db = firebase.firestore();
+//Reference to tht blog post collection created on firebase
+const blogposts = db.collection("blogposts");
 // TODO: Hook up firebase get all articles.
 // Ge all articles from firebase.
+
 router.get("/", (req, res) => {
+  const blogpostArray = []; // Push document from blogposts into the blogposts array.
+
+  //TODO: get blogposts JSON from firebase
+  res.send(blogpostArray);
   res.send(`
   <h1>All Articles</h1>
   <p>Articles would go here, need to setup firebase</p>
